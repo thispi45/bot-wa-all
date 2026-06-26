b31c5cacd32cf880427adc7f0f0d86d8137d671d
thispi45 <syoffi45@gmail.com>
2026-06-26 11:15:48 +0700

    Update package.js

--- package.js
+++ package.js
@@ -1,15 +1,16 @@
 {
-  "name": "bot-wa-all",
+  "name": "wa-bot",
   "version": "1.0.0",
+  "description": "Whatsapp Bot",
   "main": "bot.js",
   "scripts": {
     "start": "node bot.js"
   },
+  "engines": {
+    "node": "18.x"
+  },
   "dependencies": {
     "@whiskeysockets/baileys": "^6.7.5",
     "pino": "^8.17.0"
-  },
-  "engines": {
-    "node": "18.x"
   }
 }
